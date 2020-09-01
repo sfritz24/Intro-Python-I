e93e45d46cf8e71b72a9b0dce7f132cc940e6813
shanon <13fritsh@gmail.com>
2020-08-31 22:50:59 -0500

    fixed 03 and worked on 04 and 05

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,7 +10,8 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-print(sys.argv[0])
+for arg in sys.argv:
+    print(arg, end='\n')
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
